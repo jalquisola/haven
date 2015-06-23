148f736544c1f80aae1731264c00f75bcf80253b
jalquisola <joseph.alquisola@gmail.com>
2015-06-24 00:09:35 +0800

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require ./default/jquery-2.1.1.min.js
+//= require ./default/jquery-ui.min.js
+//= require ./default/jquery-ui-touch-punch.js
+//= require ./default/jquery.placeholder.js
+//= require ./default/bootstrap.js
+//= require ./default/jquery.touchSwipe.min.js
+//= require ./default/infobox.js
+//= require ./default/jquery.visible.js
+//= require ./default/home.js
+//= require jquery_ujs
