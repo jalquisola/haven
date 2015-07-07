b09b8c128bf9330664281877b949a9073a0d09fe
jalquisola <joseph.alquisola@gmail.com>
2015-07-07 21:06:12 +0800

    sign-in js

---
+++ app/assets/javascripts/sign_in.js
@@ -0,0 +1,10 @@
+//= require ./default/jquery-2.1.1.min.js
+//= require ./default/jquery-ui.min.js
+//= require ./default/jquery-ui-touch-punch.js
+//= require ./default/jquery.placeholder.js
+//= require ./default/bootstrap.js
+//= require ./default/jquery.touchSwipe.min.js
+//= require ./default/jquery.visible.js
+//= require ./default/signin.js
+//= require ./default/signup.js
+//= require jquery_ujs
