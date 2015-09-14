c36fdbfad09d80634894f5d0de7df6d672a7a70c
jalquisola <joseph.alquisola@gmail.com>
2015-09-14 14:26:18 +0800

    banner seed

---
+++ db/seeds/development/banners_seed.rb
@@ -0,0 +1,11 @@
+BASE_URL = ENV['BASE_URL']
+Banner.connection.execute("TRUNCATE table banners;")
+
+(1..6).each do |index|
+  banner = Banner.new
+  banner.image_url = "#{BASE_URL}images/bg_#{index}.jpg"
+  banner.position = index
+  banner.save!
+end
+
+
