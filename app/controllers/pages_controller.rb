122e819a0c47e1a23ca316c7cf91f0012bf16f6f
jalquisola <joseph.alquisola@gmail.com>
2015-11-05 19:55:59 +0800

    realestates url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,45 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_filter :authenticate_user!, only: [:single, :explore]
-  before_filter :set_properties, only:[ :home, :explore, :single]
-  before_filter :set_images, only:[ :home, :explore, :single]
+  before_filter :set_properties, only:[ :home ]
+  before_filter :set_images, only:[ :home ]
 
   def home
     if user_signed_in?
-      return redirect_to explore_pages_url
+      return redirect_to realestates_path
     end
-
     @banners = Banner.all
-  end
-
-  def explore
-    @properties = Property.where("short_address LIKE ?", params[:city]) if params[:city].present?
-    @properties = @properties.where(property_type: params[:property_type]) if params[:property_type].present?
-    @properties = @properties.where(status: params[:status]) if params[:status].present?
-
-    if params[:price].present?
-      price = params[:price].match(';') ? params[:price].split(';').first : "#{params[:price]}000000"
-      @properties = @properties.where("unit_price >= ?", price)
-    end
-
-    render :explore, layout: 'explore'
-  end
-
-  def single
-    @property = Property.friendly.find(params[:property_id])
-    return redirect_to root_url  unless @property
-
-
-    @property.update_attribute(:view_count, @property.view_count+1)
-    if request.referrer && request.referrer.match(/pages\/explore/)
-      @property.update_attribute(:favourite_count, @property.favourite_count+1)
-    end
-
-    @unit_types = @property.unit_types
-    @description = @property.description
-    @current_user = current_user
-    @comments = @property.comments
-    @enquiry = Enquiry.new
-    render :single, layout: 'single'
   end
 
   def dashboard
