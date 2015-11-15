a43469016b20d56eef33ef6f47611c36e488d9cf
jalquisola <joseph.alquisola@gmail.com>
2015-11-15 15:16:43 +0800

    preload resources

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,11 @@
 class PagesController < ApplicationController
-  before_filter :set_properties, only:[ :home ]
-  before_filter :set_images, only:[ :home ]
   layout 'zoner/application'
 
   def home
     if user_signed_in?
       return redirect_to realestates_path
     end
-    @banners = Banner.all
+    @properties = Property.includes(:images).all
   end
 
   def dashboard
@@ -16,14 +14,5 @@ class PagesController < ApplicationController
 
   def sellers
     render :sellers, layout: 'sellers'
-  end
-
-  private
-  def set_properties
-    @properties = Property.includes(:unit_types, :images).all
-  end
-
-  def set_images
-    @images = Image.where(property_id: @properties.map(&:id), position: 1).group_by(&:property_id)
   end
 end
--- app/controllers/realestates_controller.rb
+++ app/controllers/realestates_controller.rb
@@ -1,11 +1,10 @@
 class RealestatesController < ApplicationController
   skip_before_filter :authenticate_user!, only: [:show, :index]
-  before_filter :set_properties, only:[ :index, :show]
-  before_filter :set_images, only:[ :index, :show]
   layout 'zoner/application'
 
   def index
-    @properties = Property.where("short_address LIKE ?", params[:city]) if params[:city].present?
+    @properties = Property.includes(:images)
+    @properties = @properties.where("short_address LIKE ?", params[:city]) if params[:city].present?
     @properties = @properties.where(property_type: params[:property_type]) if params[:property_type].present?
     @properties = @properties.where(status: params[:status]) if params[:status].present?
 
@@ -16,9 +15,8 @@ class RealestatesController < ApplicationController
   end
 
   def show
-    @property = Property.friendly.find(params[:id])
+    @property = Property.friendly.includes(:agent, :unit_types, :description, :floor_plans).find(params[:id])
     return redirect_to root_url  unless @property
-
 
     @property.update_attribute(:view_count, @property.view_count+1)
     if request.referrer && request.referrer.match(/pages\/explore/)
@@ -31,15 +29,5 @@ class RealestatesController < ApplicationController
     @current_user = current_user
     @comments = @property.comments
     @enquiry = Enquiry.new
-  end
-
-  private
-
-  def set_properties
-    @properties = Property.includes(:unit_types, :images).order(id: :desc).all
-  end
-
-  def set_images
-    @images = Image.where(property_id: @properties.map(&:id), position: 1).group_by(&:property_id)
   end
 end
