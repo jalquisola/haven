6fb126bce9a8cae2fc7f0bcb040b44ebd0a80cdd
jalquisola <joseph.alquisola@gmail.com>
2015-12-29 17:28:25 +0800

    add sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get '/sitemap.xml.gz', to: redirect("https://s3.amazonaws.com/smdcinvestments.com/sitemaps/sitemap.xml.gz"), as: :sitemap
 
   get 'users/profile' => 'users#profile'
   # You can have the root of your site routed with "root"
