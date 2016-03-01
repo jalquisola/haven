723d8e97d6b4220d1b12df9d1134895edafcb4ae
jalquisola <joseph.alquisola@gmail.com>
2016-03-01 23:19:08 +0800

    update description page and remove promotions

--- app/controllers/lp_controller.rb
+++ app/controllers/lp_controller.rb
@@ -6,8 +6,13 @@ class LpController < ApplicationController
     @property = Property.friendly.includes(:agent, :unit_types, :description, :floor_plans).find(params[:id])
     return redirect_to root_url  unless @property
 
-    @title = "#{@property.name} - #{ENV['WEBSITE_TITLE']} LP"
-    @desc = "Are you looking for #{@property.name}? Learn more about the project by reading this article."
+    @title = "#{@property.name} - www.mybesthaven.com"
+    if @property.name == "Shore 2 Residences"
+      @title = "#{@property.name} - A Private Island called HOME."
+      @desc = "Located in SM Mall of Asia. Pre-selling Units at 0% over 40 months. Monthly Investment starts at Php14,000++. Inquire NOW!"
+    else
+      @desc = "Are you looking for #{@property.name}? Learn more about the project by reading this article."
+    end
     @image_url = @property.images.first.url
 
     @agent = @property.agent
